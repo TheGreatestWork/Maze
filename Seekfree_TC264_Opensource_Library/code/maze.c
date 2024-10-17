f34044a311719c917579f51230ac5751544c57a9
TheGreatestWork <143500916+TheGreatestWork@users.noreply.github.com>
2024-10-17 11:56:09 +0800

    Update maze.c

--- Seekfree_TC264_Opensource_Library/code/maze.c
+++ Seekfree_TC264_Opensource_Library/code/maze.c
@@ -5,40 +5,40 @@
 
 #include "zf_common_headfile.h"
 
-#define AMOUNT 200//×ÜÑ­»·´ÎÊı
+#define AMOUNT 200//æ€»å¾ªç¯æ¬¡æ•°
 
-#define LIN 94      //Í¼Ïñ´óĞ¡£¨³¤£©
-#define ROW 60      //Í¼Ïñ´óĞ¡£¨¿í£©
+#define LIN 94      //å›¾åƒå¤§å°ï¼ˆé•¿ï¼‰
+#define ROW 60      //å›¾åƒå¤§å°ï¼ˆå®½ï¼‰
 
-#define BLACK 0   //ºÚÉ«
-#define WHITE 255     //°×É«
+#define BLACK 0       //é»‘è‰²
+#define WHITE 255     //ç™½è‰²
 
 uint8 image[ROW][LIN];
-uint8 threshold = 150;//Í¼Ïñ¶şÖµ»¯ãĞÖµ
+uint8 threshold = 150;//å›¾åƒäºŒå€¼åŒ–é˜ˆå€¼
 
-struct LEFT_EDGE  L_edge[AMOUNT];       //×ó±ß½ç½á¹¹Ìå
-struct RIGHT_EDGE R_edge[AMOUNT];       //ÓÒ±ß½ç½á¹¹Ìå
+struct LEFT_EDGE  L_edge[AMOUNT];       //å·¦è¾¹çº¿ç»“æ„ä½“
+struct RIGHT_EDGE R_edge[AMOUNT];       //å³è¾¹çº¿ç»“æ„ä½“
 
-int grodir[5] = {0, gru, grl, grd, grr};//Éú³¤·½Ïò
+int grodir[5] = {0, gru, grl, grd, grr};//ç”Ÿé•¿æ–¹å‘
 int lledir[5][2] = {{0, 0}, {0, -1}, {1, 0}, {0, 1}, {-1, 0}};
 int lupdir[5][2] = {{0, 0}, {-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 int rridir[5][2] = {{0, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 0}};
 int rupdir[5][2] = {{0, 0}, {-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 
-int ldir_num[AMOUNT/10][4], rdir_num[AMOUNT/10][4];//ÓÃÓÚÍ³¼ÆÉú³¤·½ÏòĞÅÏ¢
+int ldir_num[AMOUNT/10][4], rdir_num[AMOUNT/10][4];//ç”¨äºç»Ÿè®¡ç”Ÿé•¿æ–¹å‘ä¿¡æ¯
 
 
 //-------------------------------------------------------------------------------------------------------------------
-// º¯Êı¼ò½é      Í¼ÏñÊı×é³õÊ¼»¯
-// ²ÎÊıËµÃ÷
-// ·µ»Ø²ÎÊı      void
-// Ê¹ÓÃÊ¾Àı      image_get();
-// ±¸×¢ĞÅÏ¢
+// å‡½æ•°ç®€ä»‹      å›¾åƒæ•°ç»„åˆå§‹åŒ–
+// å‚æ•°è¯´æ˜
+// è¿”å›å‚æ•°      void
+// ä½¿ç”¨ç¤ºä¾‹      image_get();
+// å¤‡æ³¨ä¿¡æ¯
 //-------------------------------------------------------------------------------------------------------------------
 void image_get(void)
 {
     int i = 0, j = 0, row = 0, line = 0;
-    for(i = 0; i < MT9V03X_H; i+=2)//Ñ¹ËõÍ¼Ïñ£¨60*94£©+¶şÖµ»¯´¦Àí
+    for(i = 0; i < MT9V03X_H; i+=2)//å‹ç¼©å›¾åƒï¼ˆ60*94ï¼‰+äºŒå€¼åŒ–å¤„ç†
     {
         for(j = 0; j < MT9V03X_W; j+=2)
         {
@@ -54,25 +54,25 @@ void image_get(void)
 }
 
 //--------------------------------------------------------------------------------- ----------------------------------
-// º¯Êı¼ò½é      ÃÔ¹¬·¨É¨Ïß
-// ²ÎÊıËµÃ÷
-// ·µ»Ø²ÎÊı      void
-// Ê¹ÓÃÊ¾Àı      maze();
-// ±¸×¢ĞÅÏ¢
+// å‡½æ•°ç®€ä»‹      è¿·å®«æ³•æ‰«çº¿
+// å‚æ•°è¯´æ˜
+// è¿”å›å‚æ•°      void
+// ä½¿ç”¨ç¤ºä¾‹      maze();
+// å¤‡æ³¨ä¿¡æ¯
 //-------------------------------------------------------------------------------------------------------------------
 void maze(void)
 {
     int i = 0;
     int il = 1, ir = 1;
     int count = 0;
-    int kl = 1, kr = 1;//klºÍkrµÄÈ¡Öµ·¶Î§1~4£¬¼´ÉÏ×óÏÂÓÒ
+    int kl = 1, kr = 1;//klå’Œkrçš„å–å€¼èŒƒå›´1~4ï¼Œå³ä¸Šå·¦ä¸‹å³
     int gap = 10;
     static int n = LIN/2;
     int lindex = 0, rindex = 0;
     memset(ldir_num, 0, sizeof(ldir_num));
     memset(rdir_num, 0, sizeof(rdir_num));
 /*****************************************************************************************************************************************/
-    for(i = 0; i < LIN; i++)//ÏÈ´ÓÍ¼Ïñ×îµ×ĞĞµÄÖĞ¼äÍùÁ½±ßÑ°ÕÒÌø±äµã£¬µÃµ½×óÓÒÃÔ¹¬µÄÆğµã
+    for(i = 0; i < LIN; i++)//å…ˆä»å›¾åƒæœ€åº•è¡Œçš„ä¸­é—´å¾€ä¸¤è¾¹å¯»æ‰¾è·³å˜ç‚¹ï¼Œå¾—åˆ°å·¦å³è¿·å®«çš„èµ·ç‚¹
     {
         if(image[ROW-2][n-i] == BLACK)
         {
@@ -101,17 +101,17 @@ void maze(void)
         if(lindex == 1 && rindex == 1){break;}
         if(lindex == 0)
         {
-            if(image[L_edge[il-1].row + lledir[L_edge[il-1].dir][0]][L_edge[il-1].col + lledir[L_edge[il-1].dir][1]] == BLACK)//×óÊÖÊÇ·ñÎªÇ½
+            if(image[L_edge[il-1].row + lledir[L_edge[il-1].dir][0]][L_edge[il-1].col + lledir[L_edge[il-1].dir][1]] == BLACK)//å·¦æ‰‹æ˜¯å¦ä¸ºå¢™
             {
-                if(image[L_edge[il-1].row + lupdir[L_edge[il-1].dir][0]][L_edge[il-1].col + lupdir[L_edge[il-1].dir][1]] == BLACK)//Ç°·½ÊÇ·ñÎªÇ½
+                if(image[L_edge[il-1].row + lupdir[L_edge[il-1].dir][0]][L_edge[il-1].col + lupdir[L_edge[il-1].dir][1]] == BLACK)//å‰æ–¹æ˜¯å¦ä¸ºå¢™
                 {
-                    kl--;//ÓÒ×ª
+                    kl--;//å³è½¬
                     if(kl > 4){kl = 1;}
                     if(kl < 1){kl = 4;}
                     L_edge[il-1].dir = grodir[kl];
                     il--;
                 }
-                else//Ç°½ø
+                else//å‰è¿›
                 {
                     L_edge[il].dir = L_edge[il-1].dir;
                     L_edge[il].row = L_edge[il-1].row+lupdir[L_edge[il-1].dir][0];
@@ -119,7 +119,7 @@ void maze(void)
                     ldir_num[(int)(il/gap)][kl]++;
                 }
             }
-            else//×ó×ª²¢Ç°½ø
+            else//å·¦è½¬å¹¶å‰è¿›
             {
                 kl++;
                 if(kl > 4){kl = 1;}
@@ -134,17 +134,17 @@ void maze(void)
         }
         if(rindex == 0)
         {
-            if(image[R_edge[ir-1].row + rridir[R_edge[ir-1].dir][0]][R_edge[ir-1].col + rridir[R_edge[ir-1].dir][1]] == BLACK)//ÓÒÊÖÊÇ·ñÎªÇ½
+            if(image[R_edge[ir-1].row + rridir[R_edge[ir-1].dir][0]][R_edge[ir-1].col + rridir[R_edge[ir-1].dir][1]] == BLACK)//å³æ‰‹æ˜¯å¦ä¸ºå¢™
             {
-                if(image[R_edge[ir-1].row + rupdir[R_edge[ir-1].dir][0]][R_edge[ir-1].col + rupdir[R_edge[ir-1].dir][1]] == BLACK)//Ç°·½ÊÇ·ñÎªÇ½
+                if(image[R_edge[ir-1].row + rupdir[R_edge[ir-1].dir][0]][R_edge[ir-1].col + rupdir[R_edge[ir-1].dir][1]] == BLACK)//å‰æ–¹æ˜¯å¦ä¸ºå¢™
                 {
-                    kr++;//×ó×ª
+                    kr++;//å·¦è½¬
                     if(kr > 4){kr = 1;}
                     if(kr < 1){kr = 4;}
                     R_edge[ir-1].dir = grodir[kr];
                     ir--;
                 }
-                else//Ç°½ø
+                else//å‰è¿›
                 {
                     R_edge[ir].dir = R_edge[ir-1].dir;
                     R_edge[ir].row = R_edge[ir-1].row+rupdir[R_edge[ir-1].dir][0];
@@ -152,7 +152,7 @@ void maze(void)
                     rdir_num[(int)(ir/gap)][kr]++;
                 }
             }
-            else//ÓÒ×ª²¢Ç°½ø
+            else//å³è½¬å¹¶å‰è¿›
             {
                 kr--;
                 if(kr > 4){kr = 1;}
@@ -189,23 +189,23 @@ void maze(void)
     }
 }
 
-extern int tim;//ÓÃÓÚÏÔÊ¾Ê±¼ä£¬¶¨ÒåÔÚmain.cÎÄ¼şÖĞ
+extern int tim;//ç”¨äºæ˜¾ç¤ºæ—¶é—´ï¼Œå®šä¹‰åœ¨main.cæ–‡ä»¶ä¸­
 //--------------------------------------------------------------------------------- ----------------------------------
-// º¯Êı¼ò½é
-// ²ÎÊıËµÃ÷
-// ·µ»Ø²ÎÊı      void
-// Ê¹ÓÃÊ¾Àı      tft_display();
-// ±¸×¢ĞÅÏ¢
+// å‡½æ•°ç®€ä»‹
+// å‚æ•°è¯´æ˜
+// è¿”å›å‚æ•°      void
+// ä½¿ç”¨ç¤ºä¾‹      tft_display();
+// å¤‡æ³¨ä¿¡æ¯
 //-------------------------------------------------------------------------------------------------------------------
 void tft_display(void)
 {
-    tft180_show_gray_image(0, 0, image[0], LIN, ROW, LIN, ROW, 0);//ÉãÏñÍ·
-    for(int i = 0; i < AMOUNT; i++)//±ßÏß´òÓ¡
+    tft180_show_gray_image(0, 0, image[0], LIN, ROW, LIN, ROW, 0);//æ˜¾ç¤ºäºŒå€¼åŒ–åçš„å›¾åƒ
+    for(int i = 0; i < AMOUNT; i++)//è¾¹çº¿æ‰“å°
     {
-        tft180_draw_point((uint16)L_edge[i].col, (uint16)L_edge[i].row, RGB565_RED);
-        tft180_draw_point((uint16)R_edge[i].col, (uint16)R_edge[i].row, RGB565_BLUE);
+        tft180_draw_point((uint16)L_edge[i].col, (uint16)L_edge[i].row, RGB565_RED);//çº¢è‰²æ˜¾ç¤ºå·¦è¾¹çº¿
+        tft180_draw_point((uint16)R_edge[i].col, (uint16)R_edge[i].row, RGB565_BLUE);//è“è‰²æ˜¾ç¤ºå³è¾¹çº¿
     }
     tft180_show_string(0, 80, "time:");
-    tft180_show_float (40, 80, (float)tim/1000, 2, 3);
+    tft180_show_float (40, 80, (float)tim/1000, 2, 3);//æ˜¾ç¤ºæ‰«çº¿æ‰€éœ€æ—¶é—´ï¼Œå•ä½ï¼šms
     tft180_show_string(80, 80, "ms");
 }
